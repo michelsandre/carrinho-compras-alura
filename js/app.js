c45cebf8e58899a0dc6cc39e2a95a3059e1f6e79
Andre Michels de Oliveira <michelsandre@gmail.com>
2023-10-26 13:42:59 -0300

    first issue

---
+++ js/app.js
@@ -0,0 +1,32 @@
+let elemCarrinho = document.getElementById("lista-produtos");
+let elemValorTotal = document.getElementById("valor-total");
+let valorTotal = parseFloat(elemValorTotal.textContent.replace("R$", ""));
+
+function adicionar() {
+  let elemProduto = document.getElementById("produto").value.split(" - R$");
+  let descricaoProduto = elemProduto[0];
+  let preco = parseFloat(elemProduto[1]);
+
+  let quantidade = parseInt(document.getElementById("quantidade").value);
+
+  if (quantidade > 0) {
+    elemCarrinho.innerHTML += `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${descricaoProduto} <span class="texto-azul">R$${preco}</span>
+  </section>`;
+
+    valorTotal += quantidade * preco;
+    atualizaValorTotal();
+  }
+}
+
+function limpar() {
+  while (elemCarrinho.firstChild) {
+    elemCarrinho.removeChild(elemCarrinho.firstChild);
+    valorTotal = 0;
+    atualizaValorTotal();
+  }
+}
+
+function atualizaValorTotal() {
+  elemValorTotal.textContent = `R$${valorTotal}`;
+}
